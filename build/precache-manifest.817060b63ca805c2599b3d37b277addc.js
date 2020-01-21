51e284df36462cfdef877faca82bf7a644d2b2b4
Michał Wowk <michal.wowk@dev-chop-chop.org>
2020-01-21 14:51:31 +0100

    build

---
+++ build/precache-manifest.817060b63ca805c2599b3d37b277addc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "446354d7cac862529cb3514198e94b2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6373197098d285acc32d",
+    "url": "/static/css/main.c7100457.chunk.css"
+  },
+  {
+    "revision": "a6a99c0c7bbdea79f27f",
+    "url": "/static/js/2.d5a02f09.chunk.js"
+  },
+  {
+    "revision": "6373197098d285acc32d",
+    "url": "/static/js/main.7ec85ec7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
